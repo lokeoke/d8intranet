939f41fec0f7c53fb7857a845301300d1eadb137
Pavel Loparev <pavel.loparev@propeople.com.ua>
2015-10-15 17:55:13 +0300

    Date to timestamp

--- drupal/modules/custom/intranet_fields/src/Plugin/Field/FieldType/DateIntervalWithStatusItem.php
+++ drupal/modules/custom/intranet_fields/src/Plugin/Field/FieldType/DateIntervalWithStatusItem.php
@@ -47,9 +47,9 @@ class DateIntervalWithStatusItem extends FieldItemBase {
   }
 
   public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
-    $properties['start_date'] = DataDefinition::create('datetime_iso8601')
+    $properties['start_date'] = DataDefinition::create('timestamp')
       ->setLabel(t('Start date'));
-    $properties['end_date'] = DataDefinition::create('datetime_iso8601')
+    $properties['end_date'] = DataDefinition::create('timestamp')
       ->setLabel(t('End date'));
     $properties['state'] = DataDefinition::create('string')
       ->setLabel(t('State'));
---
+++ drupal/modules/custom/intranet_helper/src/Normalizer/TermReferenceNormalizer.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\intranet_helper\Normalizer\TeamNormalizer.
+ */
+
+namespace Drupal\intranet_helper\Normalizer;
+
+use Drupal\serialization\Normalizer\EntityReferenceFieldItemNormalizer;
+
+/**
+ * Converts the Drupal entity reference item object to HAL array structure.
+ */
+class TermReferenceNormalizer extends EntityReferenceFieldItemNormalizer {
+
+  public function normalize($field_item, $format = NULL, array $context = array()) {
+    $values = array();
+
+    if (($entity = $field_item->get('entity')->getValue()) && ($entity->url('canonical'))) {
+      $values['target_id'] = $entity->id();
+      $values['name'] = $entity->name->getValue()[0]['value'];
+    }
+
+    return $values;
+  }
+
+}
