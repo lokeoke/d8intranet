9049f3c998e27bcc973f53dc55953da89627f60d
Alexander Mikhutkin <alexander.mikhutkin@propeople.com.ua>
2015-10-26 12:40:03 +0200

    Fix calculation of vacation days.

--- angular/www/scripts/employees/teamsController.js
+++ angular/www/scripts/employees/teamsController.js
@@ -12,44 +12,6 @@ angular.module('d8intranetApp')
 
       getTeamsList.getTeams(config.teamsUrl).then(function(d){
         $scope.teams = d;
-        $scope.regularTeams = {};
         $rootScope.dataLoaded = true;
-
-        console.log($scope.teams)
-
-        //angular.forEach($scope.teams, function(team) {
-        //  console.log(team);
-        //  $scope.regularTeams = team;
-        //
-        //  //angular.forEach(team, function(key, value) {
-        //  //  $scope.regularTeams[value] = key;
-        //  //})
-        //});
-
-        //console.log($scope.regularTeams);
-
       });
-
-    // Teams requests
-    //$http({
-    //  method: 'GET',
-    //  url: '../../jsons/teams.json'
-    //}).success(function (response) {
-    //  $scope.teamsList = response.teams;
-    //  $rootScope.dataLoaded = true;
-    //  $scope.regularTeams = {};
-    //  $scope.members = {};
-    //
-    //  // Created new object for all teams
-    //  // In case if lead team (director, dep lead) create separate object
-    //  // It's necessary to have separate leads and other teams for output
-    //  angular.forEach($scope.teamsList, function (team) {
-    //    angular.forEach(team, function (key, value) {
-    //      $scope.regularTeams[value] = key;
-    //    });
-    //  });
-    //});
-
-
-
   });
